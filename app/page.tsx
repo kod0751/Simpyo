a344d56b55a7485f5b1f66e5a784ae5d8e53db28
kod0751 <50727457+kod0751@users.noreply.github.com>
2026-07-19 23:04:57 +0900

    feat(auth): 구글 로그인 데스트 코드 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,13 @@
-import { createClient } from "@/lib/supabase/server";
 import { Footer } from "@/widgets/footer";
 import { Header } from "@/widgets/header";
 import { HeroSection } from "@/widgets/hero-section";
 
 export default async function Home() {
-  const supabase = await createClient();
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="flex-1">
         <HeroSection />
-        <div>
-          {user ? <p>로그인 성공: {user.email}</p> : <p>로그인 안 됨</p>}
-        </div>
       </main>
       <Footer />
     </div>
