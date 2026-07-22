17e841a8a46e9d1f426b8f961a65f4ca808a3eb9
kod0751 <zzdfpi@naver.com>
2026-07-22 16:20:27 +0900

    fix(layout): 중복된 header, footer 제거

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,15 +1,11 @@
-import { Footer } from "@/widgets/footer";
-import { HeaderWrapper } from "@/widgets/header/ui/HeaderWrapper";
 import { HeroSection } from "@/widgets/hero-section";
 
 export default async function Home() {
   return (
     <div className="flex min-h-screen flex-col">
-      <HeaderWrapper />
       <main className="flex-1">
         <HeroSection />
       </main>
-      <Footer />
     </div>
   );
 }
