853f9875e5e8a53040d7f97f3c43d7ed11ca782f
kod0751 <zzdfpi@naver.com>
2026-07-23 15:01:52 +0900

    feat(main): 메인 페이지에 marquee 섹션 추가

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,12 @@
 import { HeroSection } from "@/widgets/hero-section";
+import { Marquee } from "@/widgets/marquee";
 
 export default async function Home() {
   return (
     <div className="flex min-h-screen flex-col">
       <main className="flex-1">
         <HeroSection />
+        <Marquee />
       </main>
     </div>
   );
